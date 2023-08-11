85c4a660853e8cef16f51858d28e7736712f2d5d
Jefferson <stupidjeff430@gmail.com>
2023-08-11 20:47:13 +0900

    add

--- js/script.js
+++ js/script.js
@@ -33,7 +33,8 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["I'm an Independent Learner", "For now, I focus on web development.", "Currently, I am focused on learning", "Currently, I am focused on learning", "JS frameworks and CSS frameworks(tailwind css, react.js, next.js, etc)"],
+        // strings: ["I'm an Independent Learner", "For now, I focus on web development.", "Currently, I am focused on learning", "JS frameworks and CSS frameworks(tailwind css, react.js, next.js, etc)"],
+        strings: ["I'm a Self-Taught Front End Developer", "Currently, I am focused on learning", "JS frameworks and CSS frameworks(tailwind css, react.js, next.js, etc)"],
         typeSpeed: 50,
         backSpeed: 70,
         loop: true
