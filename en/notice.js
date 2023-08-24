afc18b1c3de97477bae173ce8c9a600b6998f527
Jefferson <stupidjeff430@gmail.com>
2023-08-24 11:38:46 +0900

    add

--- en/notice.js
+++
@@ -1,8 +0,0 @@
-
-$(document).ready(function() {
-    $('#warning').on('click', function() {
-        $('.warning').fadeOut(300, function(){
-          $('.warning').remove();
-        });
-    });
-});
